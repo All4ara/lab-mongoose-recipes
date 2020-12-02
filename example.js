5919b4b18840d39a324f768c643f6c3b64173665
Ariel Elazarov <ariel.elazarov@yahoo.com>
2020-12-02 16:19:24 -0500

    CRUD

---
+++ example.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+ 
+mongoose
+  .connect('mongodb://localhost/recipes', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
+
+const Recipe = mongoose.model('Recipe', recipeSchema, 'recipes')
+
+Recipe.create({
+    
+})
